12c78f6cdeb29cc38f03c9ca591ecbf64d0dd876
Puker Who <nawwesele@gmail.com>
2023-08-02 11:39:18 +0300

    edit

--- template-parts/search.php
+++ template-parts/search.php
@@ -3,5 +3,5 @@
   <div class="mb-6">
     <?php echo get_search_form(); ?>
   </div>
-  <div class="text-sm"><span class="font-medium"><?php _e("Example", "treba-wp"); ?></span>: <a href="#" class="underline ">Banana</a>, <a href="#" class="underline">Egg shell</a>, <a href="#" class="underline">Paper</a></div>
+  <div class="text-sm"><span class="font-medium"><?php _e("Example", "treba-wp"); ?></span>: <a href="/banana" class="underline ">Banana</a>, <a href="/egg-shell" class="underline">Egg shell</a>, <a href="/paper" class="underline">Paper</a></div>
 </div>
